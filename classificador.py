1eb526386134019a933ad695e07d2f3ad41d5550
Lucas <lucas.soares.avila@gmail.com>
2019-06-13 18:26:08 -0300

    starting the multiclass classification

--- classificador.py
+++ classificador.py
@@ -2,6 +2,7 @@ import openface
 import cv2
 import numpy as np 
 import os
+from sklearn import svm
 
 fileDir = os.path.dirname(os.path.realpath(__file__))
 modelDir = os.path.join(fileDir, 'models')
@@ -11,31 +12,54 @@ openfaceModelDir = os.path.join(modelDir, 'openface')
 align = openface.AlignDlib(os.path.join(dlibModelDir, "shape_predictor_68_face_landmarks.dat"))
 net = openface.TorchNeuralNet(os.path.join(openfaceModelDir, 'nn4.small2.v1.t7'))
 
-#search for openface.data.iterImgs
 
 def alignImg(img):
-    rgbimg = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    rgbimg = img.getRGB()
     bb = align.getLargestFaceBoundingBox(rgbimg)
     alignedFace = align.align(96, rgbimg, bb, landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
     return alignedFace
+        
 
 def main():
     persons = ["igor", "miguel", "rafael", "renan", "ricardo", "rudyer", "vinicius"]
     persons_rep = []
 
+    # Reading the persons that already know
     for person in persons:
+        imgs = list(openface.data.iterImgs("raw/"+ person))
         rep = []
-        print(person)
-        for i in range(1,6):
-            img = cv2.imread("raw/" + person + "/" + str(i) + ".jpg")
+        for i,img in enumerate(imgs,start=1):
             aligned = alignImg(img)
-            print(i)
-            aux = net.forward(aligned)
-            rep.append(aux)
+            if aligned is None:
+                line = person + " " + str(i) + "  nao reconhecida"
+                print(line)
+            else:
+                rep.append(net.forward(aligned))
         persons_rep.append(rep)
+    
+    # Reading the unknown person
+    img = openface.data.Image("unkown", "2.jpg", "raw/desconhecido").getRGB()
+    img_aligned = alignImg(img)
+    if img_aligned is None:
+        print("nao reconhecido")
+        exit()
+    else:
+        img_rep = net.forward(img_aligned)
 
-    for i in range(0,6):
-        print(person(i))
-        print(persons_rep(i))
+    # Fiting into the SVC classifier
+    samples = []
+    labels = []
+
+    for i,person in enumerate(persons_rep, start=1):
+        labels.append(i)
+        samples.append(person)
+    
+
+    
+    
+    
+
+    
+        
 
 main()
