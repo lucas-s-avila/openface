3c521cde97eaf74eb0a9566f768fdf2758eec1de
Lucas <lucas.soares.avila@gmail.com>
2019-06-15 15:02:12 -0300

    Classifing for know faces is working. Start to work on the classification of new faces.

--- classificador.py
+++ classificador.py
@@ -15,13 +15,16 @@ net = openface.TorchNeuralNet(os.path.join(openfaceModelDir, 'nn4.small2.v1.t7')
 
 def alignImg(img):
     rgbimg = img.getRGB()
+    if rgbimg is None:
+        return None
     bb = align.getLargestFaceBoundingBox(rgbimg)
     alignedFace = align.align(96, rgbimg, bb, landmarkIndices=openface.AlignDlib.OUTER_EYES_AND_NOSE)
     return alignedFace
         
 
 def main():
-    persons = ["igor", "miguel", "rafael", "renan", "ricardo", "rudyer", "vinicius"]
+    '''
+    persons = ["miguel", "rudyer"]
     persons_rep = []
 
     # Reading the persons that already know
@@ -36,30 +39,45 @@ def main():
             else:
                 rep.append(net.forward(aligned))
         persons_rep.append(rep)
-    
+    '''
     # Reading the unknown person
-    img = openface.data.Image("unkown", "2.jpg", "raw/desconhecido").getRGB()
-    img_aligned = alignImg(img)
+    imgs = list(openface.data.iterImgs("raw/vinicius"))
+    img_aligned = alignImg(imgs[4])
     if img_aligned is None:
         print("nao reconhecido")
         exit()
     else:
         img_rep = net.forward(img_aligned)
 
+
+    imgs = list(openface.data.iterImgs("raw/igor"))
+    rep1 = net.forward(alignImg(imgs[0]))
+    rep1 += rep12
+
+    imgs = list(openface.data.iterImgs("raw/rudyer"))
+    rep2 = net.forward(alignImg(imgs[0]))
+    
+    imgs = list(openface.data.iterImgs("raw/miguel"))
+    rep3 = net.forward(alignImg(imgs[0]))
+
     # Fiting into the SVC classifier
-    samples = []
-    labels = []
+    samples = [rep1, rep2, rep3]
+    labels = ["igor", "rudyer", "miguel"]
+    
+    clf = svm.SVC(gamma='scale', decision_function_shape='ovo')
+    clf.fit(samples, labels)
 
-    for i,person in enumerate(persons_rep, start=1):
-        labels.append(i)
-        samples.append(person)
-    
+    # predicting the unknown face
+    print(clf.predict(img_rep.reshape(1,-1)))
+    print(clf.support_vectors_)
 
-    
-    
-    
 
-    
+    '''
+    To do:
+        classificar todas as imagens dos rostos conhecidos dentro do svm;
+        ao ler um rosto desconhecido, achar as distâncias do rosto pros SVM's;
+        se menor ou maior que tal distância (a definir), aplicar clf.predict().
+    '''   
         
 
 main()
